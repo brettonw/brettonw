6b95d8d8d139443be9686656abe877bf1fd07c5f
Bretton Wade <bretton.wade@gmail.com>
2024-09-27 01:48:44 -0400

    master - naming tweaks eliminate unused code

--- public/quiz/modules/index.mjs
+++ public/quiz/modules/index.mjs
@@ -1,10 +1,10 @@
-import {hiraganaLexigraphicalOrder, hiraganaDictionary, hiraganaFrequencyOrder} from "./japanese.mjs";
+import {hiraganaLexicographicOrder, hiraganaDictionary, hiraganaFrequencyOrder} from "./japanese.mjs";
 import {Quiz} from "./quiz.mjs";
-import {shuffleArray} from "./utility.mjs";
+import {} from "./utility.mjs";
 
 const urlParams = new URLSearchParams(window.location.search);
 
-// sccess specific query parameters
+// access specific query parameters
 const difficulty = urlParams.has("difficulty") ? parseInt(urlParams.get("difficulty"), 10) : 4;
 const dictionaryName = urlParams.has("dictionary") ? urlParams.get("dictionary") : "hiragana";
 const keysName = urlParams.has("keys") ? urlParams.get("keys") : "random";
@@ -15,7 +15,7 @@ const dictionary = {
 
 const keys = {
     "hiragana": {
-        "lexigraphical": hiraganaLexigraphicalOrder,
+        "lexicographic": hiraganaLexicographicOrder,
         "frequency": hiraganaFrequencyOrder,
         "random": Array.shuffle (Object.keys (hiraganaDictionary)).reduce ((acc, val) => { return acc + val;},  "")
     }
--- public/quiz/modules/japanese.mjs
+++ public/quiz/modules/japanese.mjs
@@ -1,5 +1,3 @@
-let body = document.body;
-
 let vowelIndex = ["", "a", "i", "u", "e", "o"];
 let consonantIndex = ["", "k", "s", "t", "n", "h", "m", "y", "r", "w", "g", "z", "d", "b", "p"];
 let synonyms = {
@@ -27,23 +25,7 @@ let consonantMasks = [
     " xxxxx"  // "b"
 ];
 
-let maskOffsets = function () {
-    let offsets = [];
-    let offset = 0;
-    offsets.push (offset);
-    for (let i = 0, len = consonantMasks.length; i < len; i++) {
-        let mask = consonantMasks[i];
-        let maskLength = 0;
-        for (let j = 0; j < mask.length; j++) {
-            if (mask[j] === "x") ++maskLength;
-        }
-        offset += maskLength;
-        offsets.push (offset);
-    }
-    return offsets;
-} ();
-
-export let hiraganaLexigraphicalOrder =
+export let hiraganaLexicographicOrder =
     "あいうえお" + // ""
     "かきくけこ" + // "k"
     "さしすせそ" + // "s"
@@ -71,9 +53,8 @@ let makeHiraganaDictionary = function () {
             if (consonantMask[j] === "x") {
                 let assembly = consonant + vowelIndex[j];
                 if (assembly.length > 0) {
-                    let hiraganaChar = hiraganaLexigraphicalOrder[hiraganaIndex++];
-                    let hiraganaName = (assembly in synonyms) ? synonyms[assembly] + ` (${assembly})` : assembly;
-                    dictionary[hiraganaChar] = hiraganaName;
+                    let hiraganaChar = hiraganaLexicographicOrder[hiraganaIndex++];
+                    dictionary[hiraganaChar] = (assembly in synonyms) ? synonyms[assembly] + ` (${assembly})` : assembly;
                 }
             }
         }
