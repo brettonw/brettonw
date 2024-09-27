315702099d2be5cc8ebf435d41af4937b7cc52e9
Bretton Wade <bretton.wade@gmail.com>
2024-09-26 22:06:01 -0400

    master - adding parameters

--- public/quiz/modules/index.mjs
+++ public/quiz/modules/index.mjs
@@ -1,4 +1,24 @@
 import {hiraganaLexigraphicalOrder, hiraganaDictionary, hiraganaFrequencyOrder} from "./japanese.mjs";
 import {Quiz} from "./quiz.mjs";
+import {shuffleArray} from "./utility.mjs";
 
-Quiz.new ({ difficulty: 4, dictionary: hiraganaDictionary, keys: hiraganaFrequencyOrder, word: "word-content" }).start();
+const urlParams = new URLSearchParams(window.location.search);
+
+// sccess specific query parameters
+const difficulty = urlParams.has("difficulty") ? parseInt(urlParams.get("difficulty"), 10) : 4;
+const dictionaryName = urlParams.has("dictionary") ? urlParams.get("dictionary") : "hiragana";
+const keysName = urlParams.has("keys") ? urlParams.get("keys") : "random";
+
+const dictionary = {
+    "hiragana": hiraganaDictionary
+}[dictionaryName];
+
+const keys = {
+    "hiragana": {
+        "lexigraphical": hiraganaLexigraphicalOrder,
+        "frequency": hiraganaFrequencyOrder,
+        "random": Array.shuffle (Object.keys (hiraganaDictionary)).reduce ((acc, val) => { return acc + val;},  "")
+    }
+}[dictionaryName][keysName];
+
+Quiz.new ({ difficulty: difficulty, dictionary: dictionary, keys: keys, word: "word-content" }).start();
--- public/quiz/modules/utility.mjs
+++ public/quiz/modules/utility.mjs
@@ -9,6 +9,7 @@ export let shuffleArray = function (array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
 }
 
 Array.shuffle = shuffleArray;
